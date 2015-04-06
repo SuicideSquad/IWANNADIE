d88a5c0a97b043e896844ed095ce497c58e1c4e0
Clément Gillard <clement.gillard@epita.fr>
2015-04-06 12:27:47 +0200

    Moving platform script
    Created moving platform script : check test-scene
    Added several variables to the inspector :
    - offset : the offset that must be applied to the object
    - animation time : the time in which the offset must be applied in
    seconds
    - idle time 1 : time to wait after applying offset
    - idle time 2 : time to wait after going back to the starting point
    - wait : time to wait after enabling before starting animation
    - enabled : to enable or disable the movement. disbling pauses the wait
    coutdown. public member
    The script is designed such that you can make several run on the same
    object, as long as they don't try to move the platform at the same time
    ! use idle time to make sure only one script at a time is moving the
    platform

---
+++ New Unity Project/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 offset = Vector3.up * 10;
+    [SerializeField]
+    float animationTime = 5f;
+    [SerializeField]
+    float idleTime1 = 5f;
+    [SerializeField]
+    float idleTime2 = 5f;
+    [SerializeField]
+    float wait = 0f;
+
+    public new bool enabled = true;
+
+    Vector3 origin;
+    float totalAnimation1, totalAnimation2;
+    float time;
+
+    void Start()
+    {
+        origin = transform.position;
+        totalAnimation1 = animationTime + idleTime1;
+        totalAnimation2 = animationTime + idleTime2;
+        time = -wait;
+    }
+
+    void FixedUpdate()
+    {
+        if (enabled)
+        {
+            time += Time.deltaTime;
+            if (time < 0)
+                origin = transform.position;
+            time %= totalAnimation1 + totalAnimation2;
+            if (time < animationTime)
+                transform.position = Vector3.Lerp(origin, origin + offset, time / animationTime);
+            if (totalAnimation1 < time && time < totalAnimation1 + animationTime)
+                transform.position = Vector3.Lerp(origin + offset, origin, (time - totalAnimation1) / animationTime);
+        }
+    }
+}
