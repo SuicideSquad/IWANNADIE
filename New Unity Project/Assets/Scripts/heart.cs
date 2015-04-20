25a030c014570c7ed61af3e2c62dc04400496e64
Clément Gillard <clement.gillard@epita.fr>
2015-04-20 20:38:49 +0200

    Added heart
    Modelized a heart with sharp edges
    Added particles
    Implemented rotation
    Implemented jumping
    Added rigidbody for physics
    Made a prefab with all these

---
+++ New Unity Project/Assets/Scripts/heart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class heart : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * 100);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/heartRenderer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class heartRenderer : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Mathf.Sin(Time.time * 4) / 100);
+    }
+}
